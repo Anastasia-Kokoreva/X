d8acb14af1cfed26a2cf02d4ea7d21e20c97dc56
AnnaYarushnikova <annayarushnikova06@yandex.ru>
2022-10-26 20:37:07 +0300

    Added two files

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ def openFile():
     try:
         f1 = open('data.txt', 'r')
         while True:
-            data = f1.readline()
+            data = f1.readlines()
             print(data)
             if data[0] != '':
                 return data
@@ -12,20 +12,19 @@ def openFile():
     except IOError:
         print(IOError.with_traceback())
 
+def dateMonth():
+    data2 = openFile()
+    size = len(data2)
+    data1 =['31', '12', '1900']
+    for i in range(0, size, 1):
+        n = data2[i].split('-')[0]
+        print(n)
+        if int(n) < int(data1[0]):
+            data1[0] = n
+        n = data2[i].split('-')[1]
+        if int(n) < int(data1[1]):
+            data1[1] = n
+    print(data1[0], "-", data1[1])
 
-    def dateMonth():
-        data2 = openFile()
-        size = len(data2)
-        data1 =['31', '12', '1900']
-        for i in range(0, size, 1):
-            n = data2[i].split('-')[0]
-            print(n)
-            if int(n) <int(data1[0]):
-                data1[0] = n
-            n = data2[i].split('-')[1]
-            if int(n) < int(data1[1]):
-                data1[1] = n
-        print(data1[0], "-", data1[1])
 
-
-    dateMonth()
+dateMonth()
