16b63a179102f6a077510e57e6e4efa997a26d41
AnnaYarushnikova <annayarushnikova06@yandex.ru>
2022-10-26 20:08:37 +0300

    Added two files

---
+++ main.py
@@ -0,0 +1,22 @@
+def openFile():
+    try:
+        f1 = open('data.txt', 'r')
+        while True:
+            data = f1.readline().split("-")
+            print(data)
+            if data[0] == '':
+                break
+            return data
+        f1.close()
+    except IOError:
+        print(IOError.with_traceback())
+
+def dateMonth():
+    data2 = openFile()
+    print(data2)
+    data1 = ['31', '12', '1900']
+    if int(data2[0]) < int(data1[0]):
+            data1[0] = data2[0]
+    if int(data2[1]) < int(data1[1]):
+            data1[1] = data2[1]
+    print(data1[0], " - ", data1[1])
